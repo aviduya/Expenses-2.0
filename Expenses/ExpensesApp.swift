e7f06fecfcb8d9dc2cd06d251e6ca37ad52ff0ee
Anfernee Viduya <anferneev@icloud.com>
2021-11-24 16:43:47 -0800

    Initial Commit

---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Anfernee Viduya on 11/24/21.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
